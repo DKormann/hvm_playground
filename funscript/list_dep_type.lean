59d7e6cea879e00fb102243bf49d77fe6d30f2ba
DKormann <dominikormann@outlook.com>
2025-04-18 15:04:28 +0200

    list attempt

--- funscript/list_dep_type.lean
+++ funscript/list_dep_type.lean
@@ -1,13 +1,5 @@
 
 
-inductive VecList : List Nat -> Type
-| nil : VecList []
-| cons {n: Nat} {ns: List Nat} (x: Fin n) (xs: VecList ns) : VecList (n::ns)
-
-
-def eval := 22
-
-
 inductive Ty
 | int
 | string
@@ -28,18 +20,13 @@ inductive Expr: Ty -> Type
 open Expr
 
 
-
-inductive Ma: (List Ty) -> (o:Ty) -> Type
-| nil {o}: Ma [] o
-| cons {o} {as: List Ty} {i} (x: Expr (arrow i o)) (vs: Ma as o) : Ma (i::as) o
-
-
 #eval
-  let xi: Var int := ⟨"x"⟩
-  let xs: Var string := ⟨"y"⟩
 
-  let ma: Ma [int, string] int :=
-    Ma.cons (lam xi (var xi))
-    (Ma.cons (lam xs (intlit 33))
-    Ma.nil)
+
+
+
+
+
+
+
   22
